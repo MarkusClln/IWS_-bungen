72760066a5f7fa7e49fdb9afa33a572a30f70e16
sens-m <markus.coellen@ipa.fraunhofer.de>
2020-02-09 01:28:55 +0100

    swapped db names

--- Api-Gateway Übung/products/app/app.py
+++ Api-Gateway Übung/products/app/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 DB_IP = os.getenv('DB_IP')
 PORT = os.getenv('PORT')
 myclient = pymongo.MongoClient("mongodb://"+DB_IP+":27017/")
-mydb = myclient["payment"]
-mycol = mydb["payments"]
+mydb = myclient["productcatalog"]
+mycol = mydb["product"]
 
 
 @app.route('/', methods=['GET', 'POST'])
